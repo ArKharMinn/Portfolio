eecdc30b0c8c597238a69dd87499633b5219a857
ArKharMinn <arkharminn.1@gmail.com>
2024-07-03 19:40:18 +0630

    first commit

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,6 @@
-
-var typed = new Typed('.typing', {
-    strings: ['MY NAME IS AR KHAR MINN', 'I AM A WEB DEVELOPER'],
-    typeSpeed: 50,
-    backSpeed : 50,
-    loop:true, }
-)
+var typed = new Typed(".typing", {
+  strings: ["MY NAME IS AR KHAR MINN", "I AM A WEB DEVELOPER"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
